9301141448ba442b0f31650a5de625353063c925
CBStonesifer <cbstonesifer@gmail.com>
2024-11-04 00:19:06 +0000

    Adding camera features and AWS IoT for S3, and DynamoDB

--- ESP32_Wifi-Kit/src/main.cpp
+++ ESP32_Wifi-Kit/src/main.cpp
@@ -3,17 +3,77 @@
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
 #include "WiFi.h"
- 
+#include "esp_camera.h"
+// #include "camera_pins.h"
+#define CAMERA_MODEL_WROVER_KIT
+
 #define LED_BUILTIN  2
+
+#define CAMERA_MODEL_WROVER_KIT
+// #include "camera_pins.h"
  
 #define AWS_IOT_PUBLISH_TOPIC   "esp32/pub"
 #define AWS_IOT_SUBSCRIBE_TOPIC "esp32/sub"
+
  
 WiFiClientSecure net = WiFiClientSecure();
 PubSubClient client(net);
+camera_config_t config;
 
 float h;
 float t;
+
+#if defined(CAMERA_MODEL_WROVER_KIT)
+#define PWDN_GPIO_NUM    -1
+#define RESET_GPIO_NUM   -1
+#define XCLK_GPIO_NUM    21
+#define SIOD_GPIO_NUM    26
+#define SIOC_GPIO_NUM    27
+
+#define Y9_GPIO_NUM      35
+#define Y8_GPIO_NUM      34
+#define Y7_GPIO_NUM      39
+#define Y6_GPIO_NUM      36
+#define Y5_GPIO_NUM      19
+#define Y4_GPIO_NUM      18
+#define Y3_GPIO_NUM       5
+#define Y2_GPIO_NUM       4
+#define VSYNC_GPIO_NUM   25
+#define HREF_GPIO_NUM    23
+#define PCLK_GPIO_NUM    22
+#else
+#error "Camera model not selected"
+#endif
+
+
+void camera_init() {
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sccb_sda = SIOD_GPIO_NUM;
+  config.pin_sccb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.frame_size = FRAMESIZE_QVGA;
+  config.pixel_format = PIXFORMAT_JPEG; // for streaming
+  //config.pixel_format = PIXFORMAT_RGB565; // for face detection/recognition
+  config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+  config.fb_location = CAMERA_FB_IN_PSRAM;
+  config.jpeg_quality = 10;
+  config.fb_count = 2;
+}
 
 void messageHandler(char* topic, byte* payload, unsigned int length)
 {
@@ -83,8 +143,25 @@ void publishMessage()
  
 void setup()
 {
-  Serial.begin(115200);
-  connectAWS();
+    camera_init();
+
+    // camera init
+    esp_err_t err = esp_camera_init(&config);
+
+    if (err != ESP_OK) {
+        Serial.printf("Camera init failed with error 0x%x", err);
+        return;
+    }
+
+    sensor_t * s = esp_camera_sensor_get();
+    s->set_vflip(s, 0);        //1-Upside down, 0-No operation
+    s->set_hmirror(s, 0);      //1-Reverse left and right, 0-No operation
+    s->set_brightness(s, 1);   //up the blightness just a bit
+    s->set_saturation(s, -1);  //lower the saturation
+
+    Serial.begin(115200);
+
+    connectAWS();
 }
  
 void loop()
